590f8717ea453cf51d027f740b718b1f4838d112
Gruzz <Neverfall86@gmail.com>
2018-08-06 16:40:22 +0300

    1

--- INumber.cs
+++ INumber.cs
@@ -8,7 +8,15 @@ namespace Interfaces
 {
     public interface INumber
     {
+        /// <summary>
+        /// Get integer value of number
+        /// </summary>
+        /// <returns></returns>
         int GetValue();
+        /// <summary>
+        /// Set new integer value of a number
+        /// </summary>
+        /// <param name="newVal"></param>
         void UpdateValue(int newVal);        
     }
 }
